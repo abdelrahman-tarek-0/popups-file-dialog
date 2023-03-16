683e5cb7cdcf857b60f905f17f068a38afbeda93
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-16 19:09:42 +0200

    build for win

--- lib/vendors/src/cli.c
+++ lib/vendors/src/cli.c
@@ -47,55 +47,6 @@ char *replace_str(char *str, char *orig, char *rep, int start)
   return str;
 }
 
-#include <stdio.h>
-#include <string.h>
-#include "tinyfiledialog/tinyfiledialogs.h"
-
-int checkStrInArr(char x[][66], char *s)
-{
-    int i;
-    for (i = 0; i < 5; ++i)
-    {
-        if (!strcmp(x[i], s))
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-char *replace_char(char *str, char find, char replace)
-{
-    char *current_pos = strchr(str, find);
-    while (current_pos)
-    {
-        *current_pos = replace;
-        current_pos = strchr(current_pos, find);
-    }
-    return str;
-}
-
-
-char *replace_str(char *str, char *orig, char *rep, int start)
-{
-  static char temp[4096];
-  static char buffer[4096];
-  char *p;
-
-  strcpy(temp, str + start);
-
-  if(!(p = strstr(temp, orig)))  // Is 'orig' even in 'temp'?
-    return temp;
-
-  strncpy(buffer, temp, p-temp); // Copy characters from 'temp' start to 'orig' str
-  buffer[p-temp] = '\0';
-
-  sprintf(buffer + (p - temp), "%s%s", rep, p + strlen(orig));
-  sprintf(str + start, "%s", buffer);    
-
-  return str;
-}
-
 int main(int argc, char *argv[])
 {
 
