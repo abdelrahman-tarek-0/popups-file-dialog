0a72bf43e8e22a32659007deef429f3aa53cd232
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-27 04:14:31 +0200

    compilled the binarry

--- lib/vendors/gcc-build-linux.sh
+++ lib/vendors/gcc-build-linux.sh
@@ -1,1 +1,1 @@
-gc ./lib/vendors/src/cli.c ./lib/vendors/src/tinyfiledialog/tinyfiledialogs.c -o ./lib/vendors/bin/linux.app
+gcc ./lib/vendors/src/cli.c ./lib/vendors/src/tinyfiledialog/tinyfiledialogs.c -o ./lib/vendors/bin/linux.app
